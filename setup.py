269f8aaacd1673f4d5cafcc094e220bb893ca088
Viodex <viodex02@gmail.com>
2025-09-01 02:58:22 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Vine_Forcer',
     version='1.0',
-    py_modules=['Vino_Forcer'],         
+    packages=find_packages(),   # يستخدم جميع الباكيجات في المجلد
     install_requires=[
         'httpx',
     ],
     entry_points={
         'console_scripts': [
-            'vine_forcer=Vino_Forcer:main', 
+            'vine_forcer=Vino_Forcer:cli',  # دالة cli في Vino_Forcer.py
         ],
     },
+    python_requires='>=3.11',
+    description='Vine Forcer v1.0 - Hunter Mode 🕷️ - Async multi-tool for status, subdomains, brute-force, listener',
+    author='Viodex',
 )
